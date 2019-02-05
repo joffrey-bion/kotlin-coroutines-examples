a3e858d89bcd5344393ee60e3e5dc3130b6cf1c7
Joffrey Bion <joffrey.bion@gmail.com>
2019-02-05 21:26:28 +0100

    Create GenerateAsync.kt

---
+++ src/main/kotlin/com/example/coroutines/patterns/GenerateAsync.kt
@@ -0,0 +1,24 @@
+import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.async
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+//sampleStart
+fun main() = runBlocking {
+    val values = startGenerationAsync()
+    println("Printed immediately")
+    for (v in values) {
+        println("Value $v was produced")
+    }
+}
+
+fun CoroutineScope.startGenerationAsync(): ReceiveChannel<Int> = produce {
+    for (i in 0..5) {
+        delay(100)
+        channel.send(i * i)
+    }
+}
+//sampleEnd
